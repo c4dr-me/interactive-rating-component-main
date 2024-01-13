9ecf9d77b3a6d57af90b3b1895bebc4411e922f4
c4dr-me <nitrohex69@gmail.com>
2024-01-13 17:53:07 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-const ratingBubbles = document.querySelectorAll('.rating-bubble');
-ratingBubbles.forEach(bubble => {
-    bubble.addEventListener('click', function () {
-      ratingBubbles.forEach(otherBubble => {
-        if (otherBubble !== bubble) {
-          otherBubble.classList.remove('active');
-        }
-      });
-      this.classList.toggle('active');
-    });
-  });
+// const ratingBubbles = document.querySelectorAll('.rating-bubble');
+// ratingBubbles.forEach(bubble => {
+//     bubble.addEventListener('click', function () {
+//       ratingBubbles.forEach(otherBubble => {
+//         if (otherBubble !== bubble) {
+//           otherBubble.classList.remove('active');
+//         }
+//       });
+//       this.classList.toggle('active');
+//     });
+//   });
 
-document.addEventListener('DOMContentLoaded', function () {
+
     const ratingForm = document.getElementById('ratingForm');
     const thankYouSection = document.querySelector('.thank-you-card');
-    let userRating = 0;
   
-    ratingForm.addEventListener('submit', function (event) {
-      event.preventDefault();
-  
+    ratingForm.addEventListener('submit', function (submission) {
+      submission.preventDefault();
       // Hide the form and show the thank-you section
       ratingForm.style.display = 'none';
       thankYouSection.style.display = 'block';
   
       //update the selected rating in the thank-you section
       const selectedRating = document.querySelector('.selected-rating .user-rating');
-      const userRating = document.querySelector('.rating-bubble.active input').value;
-      selectedRating.textContent = userRating;
+      // const userRating = document.querySelector('.rating-bubble.active input').value;
+      const checkedRadio = document.querySelector('input.rating-btn:checked');
+
+      if (checkedRadio) {
+      selectedRating.textContent = checkedRadio.value;
+       }
     });
-  });
+
   
